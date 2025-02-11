5f7a063cdf89195ecf15950e8bde557e7ee43dce
Adam <portgasd097@gmail.com>
2025-02-11 15:22:35 +0700

    Change database.sql

--- database.sql
+++ database.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE db_merk_mobil;
 CREATE TABLE merk_mobil (
-                            id SERIAL PRIMARY KEY,
-                            nama VARCHAR(255) NOT NULL,
-                            tahun_berdiri INT,
-                            negara_asal VARCHAR(255)
+   id SERIAL PRIMARY KEY,
+   nama VARCHAR(255) NOT NULL,
+   tahun_berdiri INT,
+   negara_asal VARCHAR(255)
 );
 
